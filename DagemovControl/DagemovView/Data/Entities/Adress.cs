6982f84453a91a12924702a1318f0cf4f181f2da
dagemov <sebcoss1234@gmail.com>
2023-09-17 05:50:17 -0400

    AddUsers

--- DagemovControl/DagemovView/Data/Entities/Adress.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DagemovView.Data.Entities
-{
-    public class Adress
-    {
-        public int Id { get; set; }
-        public ICollection<Country> Countries { get; set; }
-    }
-}
--- DagemovControl/DagemovView/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DagemovView.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
